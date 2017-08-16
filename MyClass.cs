d5e696b1a3398013ef2ae1db030a4cddfae880a8
Mujib517 <mujib517@gmail.com>
2017-08-16 09:54:57 +0530

    Classes and objects

---
+++ MyClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Net.Training
+{
+    //encapsulation
+    class MyClass
+    {
+        private int _x;  //4bytes,long:8bytes
+        private int _y;
+
+        //initialization 
+        //public MyClass()
+        //{
+        //    _x = 10;
+        //    _y = 20;
+        //    //db calls
+        //    //complex operations
+        //    Console.WriteLine("Constructor invoked");
+        //}
+
+        public MyClass(int x, int y)
+        {
+            _x = x;
+            _y = y;
+            //this.x = x;
+            //this.y = y;
+        }
+
+        //Method overloading
+        //inside a single class
+        //2 or more methods with same name
+        //with different number of parameters
+        //different type of parameters
+        public void Display()
+        {
+            Console.WriteLine(_x);
+        }
+
+        public void Display(int z)
+        {
+            Console.WriteLine(z);
+        }
+
+        public void Display(string z)
+        {
+            Console.WriteLine(z);
+        }
+
+        public void Display(int z, int w)
+        {
+            Console.WriteLine(z + " " + w);
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -17,218 +17,20 @@ namespace Net.Training
         static void Main(string[] args)
         {
 
-            string str = "";
+            //instance 
+            MyClass mc = new MyClass(50, 70);
+            mc.Display();
 
-            int age = 20;
+            MyClass mc2 = new MyClass(100, 70);
+            mc2.Display();
 
-            //if (age == 20)
-            //{
-            //    str = "you are young";
-            //}
-            //else
-            //{
-            //    str = "you are awesome";
-            //}
 
-            str = age == 20 ? "You are young" : "you are awesome";
 
-            Console.WriteLine(str);
-
-            //string name = "Joseph";
-
-            //Method2(name);
-
-            //Console.WriteLine(name);
-
-            //int x = 10;
-            //int y = 20;
-
-            //int result = Add(ref x, ref y);
-
+            //4bytes +4 bytes
+            //MyClass mc = new MyClass(); //building
+            //mc.Display();
+            //int result = mc.Add(20, 30);
             //Console.WriteLine(result);
-
-            //Console.WriteLine("x is " + x);
-            //Console.WriteLine("y is " + y);
-
-
-
-            //Console.WriteLine(Add(10, 20));
-            //result += 10;
-
-            //Add(20, 20);
-            //Add(30, 20);
-
-
-            //int x = 10;
-            //int y = 20;
-
-            //x = x + 10;
-
-            //x += 10;
-            //x -= 10;
-            // x *= 10; //x=x*10;
-
-            // x /= 10; //x=x/10;
-
-            //x %= 10; //x=x%10;  0
-            //Console.WriteLine(x);
-
-            //Console.WriteLine(x >= y);
-
-            //bool myVar = false;
-            //bool myVar2 = false;
-
-            ////if(myVar==false)
-            //if (myVar || myVar2)
-            //{
-            //    Console.WriteLine("If is executed");
-            //}
-
-            /*
-             
-             P  q   p^q
-
-             0  0    0
-             0  1    0
-             1  0    0
-             1  1    1
-
-             P  q   p|q
-
-             0  0    0
-             0  1    1
-             1  0    1
-             1  1    1
-             
-             */
-
-
-            //for (int i = 0; i < arr.Length/2; i++)
-            //{
-            //    Console.WriteLine(arr[i]);
-            //}
-
-            //foreach(int elem in arr)
-            //{
-            //    Console.WriteLine(elem);
-            //}
-
-
-
-            //var enumerator = arr.GetEnumerator();
-
-            //while (enumerator.MoveNext() == true)
-            //{
-            //    Console.WriteLine(enumerator.Current);
-            //}
-
-
-            //int x = 110;
-            //int y = 20;
-
-            //int z = x % y;
-
-            //x++; //x=x+1;
-            //x--;
-
-            //int z = x++;  //post increment operator
-            //int z = ++x; //pre inc opr
-
-            //Console.WriteLine(z + " " + x);
-
-
-
-
-            //int i = 0;
-
-            //while (i < arr.Length)
-            //{
-            //    Console.WriteLine(arr[i]);
-            //    i++;
-            //}
-
-
-            //int i = 0;
-
-            //do
-            //{
-            //    Console.WriteLine(arr[i]);
-            //    i++;
-            //}
-            //while (i < arr.Length);
-
-
-            //Console.WriteLine("Enter your age");
-
-            //int age = Convert.ToInt32(Console.ReadLine())
-
-            //Console.WriteLine("Enter your name");
-
-            //string name = Console.ReadLine();
-
-            //switch (name)
-            //{
-            //    case "john":
-            //        Console.WriteLine("Hello Mr. " + name);
-            //        break;
-            //    case "susan":
-            //        Console.WriteLine("Hello Mrs. " + name);
-            //        break;
-            //}
-
-
-            //CTRL + K U
-            // CTRL + K C
-            //if (age <= 20)
-            //{
-            //    Console.WriteLine("You are young");
-            //}
-            //else if (age <= 30)
-            //{
-            //    Console.WriteLine("You are young at heart");
-            //}
-            //else if (age <= 40)
-            //{
-            //    Console.WriteLine("You are still young");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("You are awesome");
-            //}
-
-            //switch (age)
-            //{
-            //    case 20:
-            //        Console.WriteLine("You are young");
-            //        break;
-            //    case 30:
-            //        Console.WriteLine("You are young");
-            //        break;
-            //    case 40:
-            //        Console.WriteLine("You are young");
-            //        //
-            //        //
-            //        //
-            //        break;
-            //    default:
-            //        Console.WriteLine("You are awesome");
-            //        break;
-            //}
-
-            //myBool = true;
-
-            //arr[1] = 100;
-
-            //arr[5] = 300;
-
-            //Console.WriteLine(arr[0]);
-            //Console.WriteLine(arr[1]);
-            //Console.WriteLine(arr[2]);
-            //Console.WriteLine(arr[3]);
-            //Console.WriteLine(arr[4]);
-
-
-
         }
 
         static void Method1()
@@ -254,6 +56,54 @@ namespace Net.Training
         {
             name += "Mr.";
         }
-    }
 
+        static void DisplayEvenNumbers()
+        {
+            int[] vals = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //for (int i = 0; i < vals.Length; i++)
+            //{
+            //    if (vals[i] % 2 == 0)
+            //    {
+            //        Console.WriteLine(vals[i]);
+            //    }
+            //}
+
+
+            foreach (int elem in vals)
+            {
+                if (elem % 2 == 0) Console.WriteLine(elem);
+            }
+
+        }
+
+        static void DisplayOddNumbers()
+        {
+            int[] vals = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //for (int i = 0; i < vals.Length; i++)
+            //{
+            //    if (vals[i] % 2 != 0)
+            //    {
+            //        Console.WriteLine(vals[i]);
+            //    }
+            //}
+
+            //foreach (int elem in vals)
+            //{
+            //    if (elem % 2 != 0) Console.WriteLine(elem);
+            //}
+
+            int i = 0;
+            while (i < vals.Length)
+            {
+                if (vals[i] % 2 != 0)
+                {
+                    Console.Write(vals[i] + " ");
+                }
+                i++;
+            }
+            Console.WriteLine();
+        }
+    }
 }
